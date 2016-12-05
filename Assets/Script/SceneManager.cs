4b61ce30943b1b82485cf37ebfec0c6cab104550
shun4develop <shun4dev@gmail.com>
2016-12-05 19:09:15 +0900

    test

--- Assets/Script/SceneManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class SceneManager : MonoBehaviour {
-	public void ChangeScene(string sceneName){
-		UnityEngine.SceneManagement.SceneManager.LoadScene (sceneName);
-	}
-}
--- Assets/Script/SocialLoginButton.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-using MyCommon;
-public class SocialLoginButton: MonoBehaviour {
-	public void login(string type){
-		WebManager.instance.socialLogin (type);	
-	}
-}
