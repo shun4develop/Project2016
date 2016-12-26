b436a939f1f97c052405601dcbda9b92e65b1208
amano0120 <c13002@suntech.jp>
2016-12-26 12:31:09 +0900

    アルバム画面にプロフィールを追加

--- Assets/Script/UserInfoTester.cs
+++ Assets/Script/UserInfoTester.cs
@@ -6,6 +6,8 @@ using MyManagers;
 public class UserInfoTester : MonoBehaviour {
 	public InputField description;
 
+	public Text infoText;
+
 	public void updateInfomation () {
 		SaveDataManager.saveUserInfo(new UserInfomation ("original","amano","","icon"));
 		UserInfomation info = SaveDataManager.loadUserInfo ();
@@ -17,7 +19,18 @@ public class UserInfoTester : MonoBehaviour {
 			Debug.Log("ng");
 		};
 		WebManager.instance.updateUserInfomation (success,failure,info);
+	}
 
+
+	public void getUserInfo(){
+		Action<string> success = (string text) => {
+			infoText.GetComponent<Text>().text = text;
+			Debug.Log(text);
+		};
+		Action failure = ()=>{
+			Debug.Log("ng");
+		};
+		WebManager.instance.getUserInfomation(success, failure);
 	}
 
 }
