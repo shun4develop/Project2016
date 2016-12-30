58efcb39e2d4244e0fae604f957837af765d7d11
shun4develop <shun4dev@gmail.com>
2016-12-30 17:54:10 +0900

    仮止め

--- Assets/Script/DynamicGridLayout.cs
+++ Assets/Script/DynamicGridLayout.cs
@@ -6,17 +6,22 @@ using UnityEngine.UI;
 
 public class DynamicGridLayout : MonoBehaviour {
 	public int col;
+	private int childCount;
 	// Use this for initialization
 	void Start () {
 		RectTransform rt = GetComponent<RectTransform> ();
 		GridLayoutGroup gl = GetComponent<GridLayoutGroup> ();
-		float size = rt.rect.width / col;;
+		float size = rt.rect.width / col;
 		gl.cellSize = new Vector2 (size,size);
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(childCount != transform.childCount){
+			foreach (RectTransform rt in transform) {
+				rt.localScale = new Vector2 (0.95f,0.95f);
+			}
+			childCount = transform.childCount;
+		}
 	}
 }
