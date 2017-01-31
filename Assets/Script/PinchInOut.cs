60e583388b452c11a33c30f1072c23c2901816f6
amano0120 <c13002@suntech.jp>
2017-01-31 12:36:45 +0900

    全画面調整

--- Assets/Script/PinchInOut.cs
+++ Assets/Script/PinchInOut.cs
@@ -4,13 +4,7 @@ using UnityEngine.UI;
 
 public class PinchInOut : MonoBehaviour {
 
-//	public Image image;
-
 	public bool touchFlag = false;
-
-	//カメラ視覚の範囲
-//	float viewMin = 20.0f;
-//	float viewMax = 60.0f;
 
 	float vMin = 1.0f;
 	float vMax = 5.0f;
@@ -21,59 +15,18 @@ public class PinchInOut : MonoBehaviour {
 	float view = 60.0f;
 	float v = 1.0f;
 
-	Vector2 oldPos;
-	Vector2 startPos;
-	Vector2 min;
-	Vector2 max;
-
-	Rect rect;
-
-	void Start(){
-		startPos = new Vector2 (transform.position.x, transform.position.y);
-
-		rect = GetComponent<RectTransform> ().rect;
-
-	}
+	public GameObject content;
 
 	// Update is called once per frame
 	void Update () {
 		
-		min = new Vector2(rect.xMin, rect.yMin);
-		max = new Vector2(rect.xMax, rect.yMax);
-
-		Vector2 pos = transform.position;
-
-		//デバッグ用
-		if (Input.GetKey (KeyCode.UpArrow))
-			transform.position = new Vector2 (transform.position.x, transform.position.y -10);
-		if (Input.GetKey (KeyCode.DownArrow))
-			transform.position = new Vector2 (transform.position.x, transform.position.y +10);
-		if (Input.GetKey (KeyCode.LeftArrow) && pos.x > min.x)
-			transform.position = new Vector2 (transform.position.x + 10, transform.position.y);
-		if (Input.GetKey (KeyCode.RightArrow))
-			transform.position = new Vector2 (transform.position.x-10, transform.position.y);
-
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			transform.localScale= new Vector3 (transform.localScale.x + 1, transform.localScale.y+1);
-			Debug.Log (min + " / " + max);
+			Debug.Log (touchFlag);
+			content.transform.localScale= new Vector3 (transform.localScale.x + 1, transform.localScale.y+1);
 		}
-		//Vector2 pos = transform.position;
-
-		//pos.x = Mathf.Clamp (pos.x, min.x, max.x);
-		//pos.y = Mathf.Clamp (pos.y, min.y, max.y);
-
-		//transform.position = pos;
-
-
-		//Debug.Log ("x : " + transform.position.x + "y : "+ transform.position.y);
-		//Debug.Log(transform.localScale);
-
-		if (transform.localScale.x == 1 || transform.localScale.y == 1)
-			transform.position = startPos;
-
 		if (touchFlag) {
 			// マルチタッチかどうか確認
-			if (Input.touchCount >= 2) {
+			if (Input.touchCount == 2) {
 				// タッチしている２点を取得
 				Touch t1 = Input.GetTouch (0);
 				Touch t2 = Input.GetTouch (1);
@@ -97,22 +50,10 @@ public class PinchInOut : MonoBehaviour {
 					// 相対値が変更した場合、カメラに相対値を反映させる
 					if (v != 0) {
 //					map.transform.localScale = new Vector3(v, v, 1.0f);
-						transform.localScale = new Vector3 (v, v, 1.0f);
+						content.transform.localScale = new Vector3 (v, v, 1.0f);
 						backDist = newDist;
 					}
 				}
-			} else if (Input.touchCount == 1 && transform.localScale.x != 1 && transform.localScale.y != 1) {
-				Touch t = Input.GetTouch (0);
-
-				if (t.phase == TouchPhase.Began) {
-//					oldPos = t.position;
-				} else if (t.phase == TouchPhase.Moved) {
-					Vector2 deltaPos = new Vector2 (transform.position.x + t.deltaPosition.x, transform.position.y + t.deltaPosition.y);
-					transform.position = new Vector2 (deltaPos.x,deltaPos.y);
-				} else if (t.phase == TouchPhase.Ended) {
-//					oldPos = new Vector2 (0, 0);
-				}
-
 			}
 		}
 	}
@@ -123,7 +64,6 @@ public class PinchInOut : MonoBehaviour {
 
 	public void falseTouchFlag(){
 		touchFlag = false;
-		transform.position = startPos;
-		transform.localScale = new Vector3 (1, 1, 1);
+		content.transform.localScale = new Vector3 (1, 1, 1);
 	}
 }
