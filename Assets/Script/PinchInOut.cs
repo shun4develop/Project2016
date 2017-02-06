9fdd55072159f1ed5243d1059c67da14c80707d2
amano0120 <c13002@suntech.jp>
2017-02-06 14:14:34 +0900

    tes

--- Assets/Script/PinchInOut.cs
+++ Assets/Script/PinchInOut.cs
@@ -23,17 +23,24 @@ public class PinchInOut : MonoBehaviour {
 	Vector2 max;
 	Vector2 min;
 
+	public GameObject text;
+	Text tex;
+
 	void Start(){
 		startPos = transform.position;
 		rectTrans = GetComponent <RectTransform> ();
+		tex = text.GetComponent<Text> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+		tex.text = rectTrans.offsetMax.ToString () +"\n"+ rectTrans.offsetMin.ToString ();
+
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			rectTrans.sizeDelta = new Vector2 (rectTrans.sizeDelta.x+1, rectTrans.sizeDelta.y+1);
 			Debug.Log( rectTrans.offsetMax );
+			Debug.Log( rectTrans.offsetMin );
 		}
 
 		if (Input.GetKeyDown (KeyCode.E)) {
@@ -41,26 +48,46 @@ public class PinchInOut : MonoBehaviour {
 			Debug.Log( rectTrans.offsetMax );
 		}
 
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			if (rectTrans.offsetMax.y > 0 && rectTrans.offsetMin.y < 0 && rectTrans.offsetMax.x > 0 && rectTrans.offsetMin.x < 0) {
+				rectTrans.position = new Vector2 (transform.position.x - 1, transform.position.y );
+				Debug.Log ("max " + rectTrans.offsetMax);
+				Debug.Log ("min " +rectTrans.offsetMin);
+			}
+				
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			if (rectTrans.offsetMax.y >= 0 && rectTrans.offsetMin.y <= 0 && rectTrans.offsetMax.x >= 0 && rectTrans.offsetMin.x <= 0) {
+				rectTrans.position = new Vector2 (transform.position.x + 1, transform.position.y );
+				Debug.Log ("max " + rectTrans.offsetMax);
+				Debug.Log ("min " +rectTrans.offsetMin);
+			}
+		}
+
+//		rectTrans.position = new Vector2 (
+//			Mathf.Clamp(rectTrans.position.x, min, max),
+//			Mathf.Clamp(rectTrans.position.y, min, max)
+//		);
+
+		if (rectTrans.offsetMax.x <= 0) {
+
+			rectTrans.offsetMax = new Vector2 (0, rectTrans.offsetMax.y);
+			//rectTrans.offsetMin = new Vector2 (rectTrans.offsetMin.x - rectTrans.offsetMax.x, rectTrans.offsetMin.y);
+		}
+
+		if (rectTrans.offsetMin.x > 0) {
+
+			rectTrans.offsetMin = new Vector2 (0, rectTrans.offsetMax.y);
+			//rectTrans.offsetMax = new Vector2 (rectTrans.offsetMax.x - rectTrans.offsetMin.x, rectTrans.offsetMin.y);
+		}
+
 		if (touchFlag) {
 			if (Input.touchCount == 1 && rectTrans.sizeDelta.x > 0) {
 				Touch touch = Input.GetTouch (0);
 
-				max = rectTrans.offsetMax;
-				min = rectTrans.offsetMin;
-
-				if (max.y >= 0 && min.y <= 0&& max.x >= 0 && min.x <= 0) {
+				if (rectTrans.offsetMax.y >= 0 && rectTrans.offsetMin.y <= 0&& rectTrans.offsetMax.x >= 0 && rectTrans.offsetMin.x <= 0) {
 					rectTrans.position = new Vector2 (transform.position.x + touch.deltaPosition.x, transform.position.y + touch.deltaPosition.y);
-				}else{
-				if (max.y < 0) {
-					max.y = 0;
-				}else if (max.x < 0) {
-					max.x = 0;
-				}
-				else if (min.x > 0) {
-					max.x = 0;
-				}
-				else if (min.y > 0) {
-					max.y = 0;
 				}
 			}
 			// マルチタッチかどうか確認
