212f9e5f73d696243d580ba8f170e9d5bd0fb8fa
amano0120 <c13002@suntech.jp>
2017-02-08 17:03:59 +0900

    拡大縮小修正

--- Assets/Script/PinchInOut.cs
+++ Assets/Script/PinchInOut.cs
@@ -8,13 +8,13 @@ public class PinchInOut : MonoBehaviour {
 
 	NavMeshAgent agent;
 
-	float vMin = 1.0f;
-	float vMax = 5.0f;
+	float vMin = -10.0f;
+	float vMax = 10.0f;
 
 	//直前の2点間の距離.
 	private float backDist = 0.0f;
 	//初期値
-	float view = 60.0f;
+	//float view = 60.0f;
 	float v = 1.0f;
 
 	Vector2 startPos;
@@ -23,77 +23,44 @@ public class PinchInOut : MonoBehaviour {
 	Vector2 max;
 	Vector2 min;
 
-	public GameObject text;
-	Text tex;
 
 	void Start(){
 		startPos = transform.position;
 		rectTrans = GetComponent <RectTransform> ();
-		tex = text.GetComponent<Text> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		tex.text = rectTrans.offsetMax.ToString () +"\n"+ rectTrans.offsetMin.ToString ();
-
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			rectTrans.sizeDelta = new Vector2 (rectTrans.sizeDelta.x+1, rectTrans.sizeDelta.y+1);
-			Debug.Log( rectTrans.offsetMax );
-			Debug.Log( rectTrans.offsetMin );
-		}
-
-		if (Input.GetKeyDown (KeyCode.E)) {
-			rectTrans.sizeDelta = new Vector2 (rectTrans.sizeDelta.x-1, rectTrans.sizeDelta.y-1);
-			Debug.Log( rectTrans.offsetMax );
-		}
-
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			if (rectTrans.offsetMax.y > 0 && rectTrans.offsetMin.y < 0 && rectTrans.offsetMax.x > 0 && rectTrans.offsetMin.x < 0) {
-				rectTrans.position = new Vector2 (transform.position.x - 1, transform.position.y );
-				Debug.Log ("max " + rectTrans.offsetMax);
-				Debug.Log ("min " +rectTrans.offsetMin);
-			}
-				
-		}
-
-		if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			if (rectTrans.offsetMax.y >= 0 && rectTrans.offsetMin.y <= 0 && rectTrans.offsetMax.x >= 0 && rectTrans.offsetMin.x <= 0) {
-				rectTrans.position = new Vector2 (transform.position.x + 1, transform.position.y );
-				Debug.Log ("max " + rectTrans.offsetMax);
-				Debug.Log ("min " +rectTrans.offsetMin);
-			}
-		}
-
-//		rectTrans.position = new Vector2 (
-//			Mathf.Clamp(rectTrans.position.x, min, max),
-//			Mathf.Clamp(rectTrans.position.y, min, max)
-//		);
-
-		if (rectTrans.offsetMax.x <= 0) {
-
-			rectTrans.offsetMax = new Vector2 (0, rectTrans.offsetMax.y);
-			//rectTrans.offsetMin = new Vector2 (rectTrans.offsetMin.x - rectTrans.offsetMax.x, rectTrans.offsetMin.y);
-		}
-
-		if (rectTrans.offsetMin.x > 0) {
-
-			rectTrans.offsetMin = new Vector2 (0, rectTrans.offsetMax.y);
-			//rectTrans.offsetMax = new Vector2 (rectTrans.offsetMax.x - rectTrans.offsetMin.x, rectTrans.offsetMin.y);
-		}
+		if(rectTrans.sizeDelta.x == 0)
+			startPos = transform.position;
 
 		if (touchFlag) {
+			if (rectTrans.offsetMax.y <= 0) {
+				rectTrans.anchoredPosition = new Vector2 (rectTrans.anchoredPosition.x, rectTrans.anchoredPosition.y + 5f);
+			}
+			if (rectTrans.offsetMax.x <= 0) {
+				rectTrans.anchoredPosition = new Vector2 (rectTrans.anchoredPosition.x + 5f, rectTrans.anchoredPosition.y);
+			}
+			if (rectTrans.offsetMin.x >= 0) {
+				rectTrans.anchoredPosition = new Vector2 (rectTrans.anchoredPosition.x - 5f, rectTrans.anchoredPosition.y);
+			}
+			if (rectTrans.offsetMin.y >= 0) {
+				rectTrans.anchoredPosition = new Vector2 (rectTrans.anchoredPosition.x, rectTrans.anchoredPosition.y - 5f);
+			}
 			if (Input.touchCount == 1 && rectTrans.sizeDelta.x > 0) {
 				Touch touch = Input.GetTouch (0);
 
-				if (rectTrans.offsetMax.y >= 0 && rectTrans.offsetMin.y <= 0&& rectTrans.offsetMax.x >= 0 && rectTrans.offsetMin.x <= 0) {
-					rectTrans.position = new Vector2 (transform.position.x + touch.deltaPosition.x, transform.position.y + touch.deltaPosition.y);
+				Vector2 tmpPos = rectTrans.anchoredPosition;
+
+				if (rectTrans.offsetMax.y > 0 && rectTrans.offsetMin.y < 0&& rectTrans.offsetMax.x > 0 && rectTrans.offsetMin.x < 0) {
+					rectTrans.anchoredPosition = new Vector2 (rectTrans.anchoredPosition.x + touch.deltaPosition.x, rectTrans.anchoredPosition.y + touch.deltaPosition.y);
 				}
 			}
 			// マルチタッチかどうか確認
 			if (Input.touchCount >= 2) {
 				// タッチしている２点を取得
-				Touch t1 = Input.GetTouch (0);
+				Touch t1 = Input.GetTouch (0); 
 				Touch t2 = Input.GetTouch (1);
 
 				//2点タッチ開始時の距離を記憶
@@ -102,8 +69,8 @@ public class PinchInOut : MonoBehaviour {
 				} else if (t1.phase == TouchPhase.Moved && t2.phase == TouchPhase.Moved) {
 					// タッチ位置の移動後、長さを再測し、前回の距離からの相対値を取る。
 					float newDist = Vector2.Distance (t1.position, t2.position);
-					view = view + (backDist - newDist) / 100.0f;
-					v = v + (newDist - backDist) / 1000.0f;
+					//view = view + (backDist - newDist) / 100.0f;
+					v = v + (newDist - backDist) / 0.1f;
 
 					// 限界値をオーバーした際の処理
 					if (v > vMax) {
@@ -115,11 +82,11 @@ public class PinchInOut : MonoBehaviour {
 					// 相対値が変更した場合、カメラに相対値を反映させる
 					if (v != 0) {
 						//transform.localScale = new Vector3 (v, v, 1.0f);
-						if(backDist<newDist && rectTrans.sizeDelta.x <= 200){
-							rectTrans.sizeDelta = new Vector2 (rectTrans.sizeDelta.x + v, rectTrans.sizeDelta.y + v);
+						if(backDist<newDist && rectTrans.sizeDelta.x <= 400){
+							rectTrans.sizeDelta = new Vector2 (rectTrans.sizeDelta.x + v*2f, rectTrans.sizeDelta.y + v*2f);
 						}
-						if(backDist>newDist && rectTrans.sizeDelta.x >= 0){
-							rectTrans.sizeDelta = new Vector2 (rectTrans.sizeDelta.x - v, rectTrans.sizeDelta.y - v);
+						if(backDist>newDist && rectTrans.sizeDelta.x > 0){
+							rectTrans.sizeDelta = new Vector2 (rectTrans.sizeDelta.x + v*2f, rectTrans.sizeDelta.y + v*2f);
 						}
 						backDist = newDist;
 					}
